9073c81de49e051e9b6618077519bcf78751fb5f
Gary Kang <garyzkang@gmail.com>
2019-12-30 21:38:51 -0500

    Updates

---
+++ precache-manifest.f76406ab8386a95dc265d9c48ea64a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3af8fea65722ae4816314f183839b8",
+    "url": "/foolsjourney/index.html"
+  },
+  {
+    "revision": "44c2b4e51bdcd1cad376",
+    "url": "/foolsjourney/static/css/main.a3558502.chunk.css"
+  },
+  {
+    "revision": "21c013bea0b5b99745dd",
+    "url": "/foolsjourney/static/js/2.b23bc7bf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/foolsjourney/static/js/2.b23bc7bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "44c2b4e51bdcd1cad376",
+    "url": "/foolsjourney/static/js/main.a34791fd.chunk.js"
+  },
+  {
+    "revision": "873c1183fe457db4321e",
+    "url": "/foolsjourney/static/js/runtime-main.a30cfb2b.js"
+  }
+]);
